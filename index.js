bb491bb5bf4a69b46431291dd7d0c50e85d72107
Jack Funck <jackfunck21@gmail.com>
2021-02-15 09:10:21 -0500

    got all the questions to be prompted

--- index.js
+++ index.js
@@ -6,15 +6,57 @@ const Intern = require('./lib/Intern');
 const questions = require('./questions');
 
 
-
-const managerArr = [];
-const engineereArr = [];
-const InternArr = [];
-
+function qStart () {
+    console.log("Wellcome to the Team Profile Generator! Please input for the following questions")
 inquire.prompt(questions.questionsStart).then(data => {
     
 
+    manStart();
+   
 });
+
+
+}
+
+function manStart () {
+    console.log("You are now in Manager questions")
+ inquire.prompt(questions.managerQuest).then(data => {
+ internStart();
+   });
+
+}
+
+
+function internStart () {
+    console.log("You are now in Intern questions");
+    inquire.prompt(questions.internQuest).then(data =>{
+
+        engeneerStart()
+
+    });
+}
+
+function engeneerStart (){
+    console.log("You are now in Engineer questions");
+    inquire.prompt(questions.engineerQuest).then(data =>{
+        newMember();
+    });
+}
+
+function newMember () {
+inquire.prompt(questions.newMemberQ);
+
+}
+
+qStart();
+
+
+
+
+
+
+
+
 
 
 
--- questions.js
+++ questions.js
@@ -10,79 +10,80 @@ const questionsStart = [
 const managerQuest = [
     {
         type: "input",
-        message: "What is your name?",
+        message: "What is the Managers name?",
         name: "name"
       },
       {
         type: "input",
-        message: "What is your office number?",
+        message: "What is Managers office number?",
         name: "officeNumber"
       },
       {
         type: "input",
-        message: "What is your email address?",
+        message: "What is Managers email address?",
         name: "email"
       },
       {
         type: "input",
-        message: "What is your ID number?",
+        message: "What is Managers ID number?",
         name: "id"
       }
 
 ];
-
+// interns questions
 const internQuest = [
     {
         type: "input",
-        message: "What is this member's name?",
+        message: "What is this Interns's name?",
         name: "name"
       },
       {
         type: "input",
-        message: "What is this member's ID number?",
+        message: "What is this Interns's ID number?",
         name: "id"
       },
       {
         type: "input",
-        message: "What is this member's email address?",
+        message: "What is this Intern's email address?",
         name: "email"
       },
       {
         type: "input",
-        message: "Which school did this member go to?",
+        message: "Which school did this Intern go to?",
         name: "school"
       }
 ];
-
+// engineere questions
 const engineerQuest = [
     {
         type: "input",
-        message: "What is this member's name?",
+        message: "What is this Engeneer's name?",
         name: "name"
       },
       {
         type: "input",
-        message: "What is this member's ID number?",
+        message: "What is this Engeneer's ID number?",
         name: "id"
       },
       {
         type: "input",
-        message: "What is this member's email address?",
+        message: "What is this Engeneer's email address?",
         name: "email"
       },
       {
         type: "input",
-        message: "What is this member's GitHub username?",
+        message: "What is this Engeneer's GitHub username?",
         name: "github"
       }
 ];
-
+//Asking if you would like to add another team member
 const newMemberQ = [{
     type: "list",
     message: "Would you like to enter another team member?",
     name: "add",
     choices: ["yes", "no"]
 }]
+
     module.exports ={
         questionsStart,
         managerQuest,
